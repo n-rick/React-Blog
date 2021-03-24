95e57995a9ea3ada9cdf611a7622b7365e722dc2
Enrick Rico <e.ficadiere@gmail.com>
2021-03-24 14:32:41 +0100

    initilisation 1

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <header>
+      <nav>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+        </nav>
+    </header>
+    <main>
+      <a href=""></a>
+      <section>
+        <p>
+          <img src="" alt=""/>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo porro, ab doloribus placeat aperiam dolores quisquam officia veniam assumenda enim dignissimos. Cum, nemo optio? Quia qui assumenda error modi neque!
+          A odio veritatis eligendi quibusdam recusandae laboriosam repudiandae impedit, vitae expedita atque in laudantium enim quos necessitatibus blanditiis. Ea, enim eaque vitae sequi perferendis ex quia rem! Facere, recusandae atque.
+          Repellat sint modi porro eveniet magni quod optio amet totam fugit pariatur velit quo molestiae dicta recusandae nisi suscipit exercitationem sequi culpa, excepturi, esse dolorum sapiente repudiandae deleniti accusamus. Facere?
+          </p>
+      </section>
+    </main>
+    <footer>
+      <section>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+      </section>
+      <section>
+        <form>
+          <input type="text" name="" id=""/>
+          <button></button>
+        </form>
+      </section>
+    </footer>
+    </>
+  );
+}
+
+export default App;
